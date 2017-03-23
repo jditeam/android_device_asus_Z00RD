d16041d0e3a0c4d31c5139b02b4856e2cb9ad219
audahadi <wan.audahadi@gmail.com>
2017-03-23 09:02:43 +0530

    re-arrange product,call cm config last

--- full_Z00RD.mk
+++ full_Z00RD.mk
@@ -17,6 +17,10 @@
 # Inherit from Z00RD device
 $(call inherit-product, device/asus/Z00RD/device.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := full_Z00RD
 PRODUCT_DEVICE := Z00RD
