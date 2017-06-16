dcbf4e3ca8bcda73dcb55dda0ea0fd29f37f72cc
audahadi <wan.audahadi@gmail.com>
2017-06-17 02:42:13 +0530

    unified for all roms

--- slim.mk
+++ slim.mk
@@ -1,12 +1,12 @@
 # Inherit from device specific
 $(call inherit-product, device/asus/Z00RD/full_Z00RD.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/asus/msm8916-common/overlay/lineage
+DEVICE_PACKAGE_OVERLAYS += device/asus/msm8916-common/overlay/slim
 
-PRODUCT_NAME := lineage_Z00RD
+PRODUCT_NAME := slim_Z00RD
 PRODUCT_DEVICE := Z00RD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone 2 Selfie
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo slim_Z00RD-userdebug
+add_lunch_combo aokp_Z00RD-userdebug
