f3def6e85af186c3f55ff9107c1c1b1de0e53e4d
Anomalchik <vidslava@yandex.ru>
2017-09-07 17:09:04 +0300

    override vendor props

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -33,6 +33,8 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -75,6 +77,48 @@ static int read_file2(const char *fname, char *data, int max_size)
     return 1;
 }
 
+static void init_alarm_boot_properties()
+{
+    int boot_reason;
+    FILE *fp;
+
+    fp = fopen("/proc/sys/kernel/boot_reason", "r");
+    fscanf(fp, "%d", &boot_reason);
+    fclose(fp);
+
+    /*
+     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+     * For existing PMIC chips, the following mapping applies
+     * for the value of boot_reason:
+     *
+     * 0 -> unknown
+     * 1 -> hard reset
+     * 2 -> sudden momentary power loss (SMPL)
+     * 3 -> real time clock (RTC)
+     * 4 -> DC charger inserted
+     * 5 -> USB charger inserted
+     * 6 -> PON1 pin toggled (for secondary PMICs)
+     * 7 -> CBLPWR_N pin toggled (for external power supply)
+     * 8 -> KPDPWR_N pin toggled (power key pressed)
+     */
+     if (boot_reason == 3) {
+        property_set("ro.alarm_boot", "true");
+     } else {
+        property_set("ro.alarm_boot", "false");
+     }
+}
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
 void vendor_load_properties()
 {
 
@@ -90,7 +134,7 @@ void vendor_load_properties()
     }
 
     /* Z00RD  */
-    if (raw_id==1797) {
+//    if (raw_id==1797) {
 
     /* Device Setting */
     family = "WW_Phone";
@@ -106,9 +150,9 @@ void vendor_load_properties()
     sprintf(b_fingerprint, "asus/%s/ASUS_%s:6.0.1/MMB29P/13.10.10.44-20161026:user/release-keys", family, device);
     sprintf(p_carrier, "US-ASUS_%s-%s", device, family);
 
-    property_set("ro.product.device", "ASUS_Z00RD_1");
-    property_set("ro.product.model", "ASUS_Z00RD");
-    property_set("ro.build.product", "ZE500KG");
+    property_override("ro.product.device", "ASUS_Z00RD_1");
+    property_override("ro.product.model", "ASUS_Z00RD");
+    property_override("ro.build.product", "ZE500KG");
 
     /* Heap Set */
     property_set("dalvik.vm.heapstartsize", heapstartsize);
@@ -118,9 +162,9 @@ void vendor_load_properties()
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", "2m");
 
-    } else {
+/*    } else {
         property_set("ro.product.model", "Zenfone"); // this should never happen.
     }
-
+*/
     INFO("Setting build properties for %s device of %s family\n", device, family);
 }
